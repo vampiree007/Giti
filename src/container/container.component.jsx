593e7e63ae5f64eefd5bb92cff30386924aafaff
Aniket Yadav <gaurav.aniket1@gmail.com>
2022-03-06 06:26:24 +0530

    feat(responsiveness details fix - home done)

--- src/container/container.component.jsx
+++ src/container/container.component.jsx
@@ -1,24 +1,26 @@
-import { Container, Grid } from '@mui/material';
-import React from 'react'
-import Header from '../component/header/header.component'
-import Sidebar from '../component/sidebar/sidebar.component';
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import Header from "../component/header/header.component";
+import Sidebar from "../component/sidebar/sidebar.component";
 
 const ContainerComponent = ({ children, header, sidebar }) => {
-    return (
-        <div>
-            {header && <Header />}
-            <Container style={{marginTop: '20px'}} disableGutters>
-                <Grid container spacing={1}>
-                    {
-                        sidebar && <Grid item xs={2}><Sidebar /> </Grid>
-                    }
-                    <Grid item xs={sidebar ? 10 : 12}>
-                        {children}
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div>
+      {header && <Header />}
+      <Container style={{ marginTop: "20px" }}>
+        <Grid container spacing={1}>
+          {sidebar && (
+            <Grid item xs={2} display={{ xs: "none", md: "block" }}>
+              <Sidebar />{" "}
+            </Grid>
+          )}
+          <Grid item md={sidebar ? 10 : 12} xs={12}>
+            {children}
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default ContainerComponent;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,19 @@
-import ContainerComponent from '../container/container.component';
-import StickyHeadTable from '../component/table/table.component';
-
+import ContainerComponent from "../container/container.component";
+import StickyHeadTable from "../component/table/table.component";
+import { Grid } from "@mui/material";
+import Sidebar from "../component/sidebar/sidebar.component";
 
 const HomePage = () => {
-    return <div>
-        <ContainerComponent header={true} sidebar={true}>
-            <StickyHeadTable />
-        </ContainerComponent>
+  return (
+    <div>
+      <ContainerComponent header={true} sidebar={true}>
+        <Grid item xs={12} md={4} display={{ xs: "block", md: "none" }} mb={2}>
+          <Sidebar />
+        </Grid>
+        <StickyHeadTable />
+      </ContainerComponent>
     </div>
-}
+  );
+};
 
 export default HomePage;
